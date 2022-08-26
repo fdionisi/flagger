8f9d7595f49786c8446bbd760608eed6b3b6a7ee
Federico Dionisi <code@fdionisi.me>
2022-08-26 11:53:13 +0200

    refactor: remove unnecessary code

--- crates/impl/aws-identity-provider/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use flagger_types::identity_provider::{Identity, IdentityProvider};
-
-pub struct AwsIdentityProvider;
-
-impl IdentityProvider for AwsIdentityProvider {
-    fn verify(_token: String) -> Identity {
-        todo!()
-    }
-}
